50596216d497b1b62648fd7d40e7989b862b535c
Vladimir Kulikov <vladimir_kulikov@epam.com>
2023-04-21 17:57:55 +0300

    add primary and secondary styles

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,20 +3,29 @@ import styles from "./Button.module.css";
 
 const Button = (props) => {
   const classes = cn(styles.button, {
+    [styles["button--primary"]]: props.primary,
+    [styles["button--secondary"]]: !props.primary,
     [styles["button--large"]]: props.size === "large",
     [styles["button--medium"]]: props.size === "medium",
   });
 
   return (
     <button
-      type="button"
+      type={props.type}
       className={classes}
       onClick={props.onClick}
-      data-testid="search-btn"
+      style={props.style}
+      data-testid="btn"
     >
       {props.label}
     </button>
   );
+};
+
+Button.defaultProps = {
+  primary: true,
+  size: "medium",
+  type: "button",
 };
 
 export default Button;
